ab18485b76630ae9ff92cbad0d79acc3ec1bb272
Aniketdalvi385 <aniketdalvi385@gmail.com>
2023-10-12 14:36:04 +0530

    Adding files with git ignore file

---
+++ Task Up app/server/data/tasks.js
@@ -0,0 +1,74 @@
+const tasks = [
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "personal",
+        "name": "Meditation",
+        "date": "2023-12-01",
+        "time": "06:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "personal",
+        "name": "Gym",
+        "date": "2023-03-02",
+        "time": "06:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "shopping",
+        "name": "Trousers",
+        "date": "2023-4-05",
+        "time": "08:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "Work",
+        "name": "test task update",
+        "date": "2023-03-03",
+        "time": "08:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "wishlist",
+        "name": "Walk",
+        "date": "2022-12-21",
+        "time": "16:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "personal",
+        "name": "Family",
+        "date": "2022-12-28",
+        "time": "18:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "personal",
+        "name": "test task update",
+        "date": "2022-12-28",
+        "time": "06:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "personal",
+        "name": "Lunch",
+        "date": "2023-04-02",
+        "time": "02:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "work",
+        "name": "Meeting",
+        "date": "2023-06-18",
+        "time": "12:00",
+    },
+    {
+        "userId": "651ea4d408580c3355ff8baf",
+        "type": "personal",
+        "name": "test task update",
+        "date": "2023-07-18",
+        "time": "10:00",
+    },
+]
+
+export default tasks
